18923d72e8cf4ec10549ae02ded6981ca4e00ad3
Roman <roman.levinta@gmail.com>
2023-11-29 21:15:49 +0100

    Refactor admin.py, templates, urls.py, and views.py

--- blog/admin.py
+++ blog/admin.py
@@ -11,6 +11,12 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 
-admin.site.register(Post, PostAdmin)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "tags", "date",)
+    list_display = ("title", "date", "author",)
+    prepopulated_fields = {"slug": ("title",)}
+
+
 admin.site.register(Author)
 admin.site.register(Tag)
--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,50 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
 # Create your views here.
 
 
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3]
-    return render(request, "blog/index.html", {
-      "posts": latest_posts
-    })
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/all-posts.html"
+    context_object_name = "all_posts"
+    ordering = "-date"
 
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html", {
-      "all_posts": all_posts
-    })
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post-detail.html"
+    context_object_name = "post"
+    slug_url_kwarg = "slug"
+    # queryset = Post.objects.all().prefetch_related("tags")
 
 
-def post_detail(request, slug):
-    identified_post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {
-      "post": identified_post,
-      "post_tags": identified_post.tags.all()
-    })
+class StartingPageView(ListView):
+    model = Post
+    template_name = "blog/index.html"
+    context_object_name = "posts"
+    ordering = "-date"
+    paginate_by = 3
+
+# def starting_page(request):
+#     latest_posts = Post.objects.all().order_by("-date")[:3]
+#     return render(request, "blog/index.html", {
+#       "posts": latest_posts
+#     })
+
+
+# def posts(request):
+#     all_posts = Post.objects.all().order_by("-date")
+#     return render(request, "blog/all-posts.html", {
+#       "all_posts": all_posts
+#     })
+
+
+# def post_detail(request, slug):
+#     identified_post = get_object_or_404(Post, slug=slug)
+#     return render(request, "blog/post-detail.html", {
+#       "post": identified_post,
+#       "post_tags": identified_post.tags.all()
+#     })
